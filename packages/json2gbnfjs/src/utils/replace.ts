8b8aa2963338fb7203bdbfb333fc9111252e6c81
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-04-27 10:25:18 -0400

    Remove Mustache dependency (#28)

--- packages/json2gbnfjs/src/utils/replace.ts
+++ packages/json2gbnfjs/src/utils/replace.ts
@@ -1,8 +1,16 @@
-import Mustache from 'mustache';
 import { KEYS, } from '../constants/grammar-keys.js';
+
+const GBNF_KEY_REPLACEMENT_PATTERN = /{{(.*?)}}/g;
+
 export const replace = (def: unknown): string => {
   if (typeof def !== 'string') {
     throw new Error(`Expected string for ${JSON.stringify(def)}`);
   }
-  return Mustache.render(def, KEYS);
+  return def.trim().replace(GBNF_KEY_REPLACEMENT_PATTERN, (_, inner: string) => {
+    const key = KEYS[inner];
+    if (!key) {
+      throw new Error(`Unknown key ${inner} for def ${def}`);
+    }
+    return key;
+  });
 };
