1091a18fdcd1104218508948a00cd7de9d6cd7ec
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-05-03 09:05:51 -0400

    Exploring web-llm in a dev environment (#56)

--- packages/autogrammer/javascript/src/grammars/get-grammar.ts
+++ packages/autogrammer/javascript/src/grammars/get-grammar.ts
@@ -1,8 +1,10 @@
 import { SupportedLanguage, } from "../types.js";
-import Mustache from 'mustache';
+// import Mustache from 'mustache';
+// import JSON2GBNF from 'json2gbnf';
 // import type { Grammar } from 'contort';
-import SQL_GRAMMAR from './sql.gbnf?raw' assert { type: 'text' };
-import JSON_GRAMMAR from './json.gbnf?raw' assert { type: 'text' };
+// import SQL_GRAMMAR from './sql.gbnf?raw' assert { type: 'text' };
+// import JSON_GRAMMAR from './json.gbnf?raw' assert { type: 'text' };
+// import { type JSONSchema, } from "json2gbnf";
 // import PYTHON_GRAMMAR from './python.gbnf?raw';
 // import JAVASCRIPT_GRAMMAR from './javascript.gbnf?raw';
 
@@ -17,42 +19,49 @@ export function isLanguage<L extends SupportedLanguage>(language: SupportedLangu
   return language === testingLanguage;
 }
 
-function parseSQLVariables(variables: Variables_SQL | string): Variables_SQL {
-  if (typeof variables === 'string') {
-    return {};
-  }
-  return variables;
-};
+// function parseSQLVariables(variables: Variables_SQL | string): Variables_SQL {
+//   if (typeof variables === 'string') {
+//     return {};
+//   }
+//   return variables;
+// };
 
-function buildGrammar<L extends SupportedLanguage>(language: L, grammarTemplate: string, variables: Variables<L> = {}) {
-  if (isLanguage(language, 'sql')) {
-    const sqlVariables = Object.entries(parseSQLVariables(variables));
-    return Mustache.render(grammarTemplate, sqlVariables.reduce((obj, [key, value,]) => {
-      if (value.length === 0) {
-        return obj;
-      }
-      return {
-        ...obj,
-        [key]: value.map(v => JSON.stringify(v)).join(" | "),
-      };
-    }, {
-      selectlist: 'string',
-      tablename: 'string',
-    }));
-  }
-  return grammarTemplate;
-};
 
-const GRAMMARS: Record<SupportedLanguage, string> = {
-  sql: SQL_GRAMMAR as string,
-  // javascript: JAVASCRIPT_GRAMMAR,
-  // python: PYTHON_GRAMMAR,
-  json: JSON_GRAMMAR as string,
-};
+// function buildGrammar(language: 'sql', variables: Variables<'sql'>): string;
+// function buildGrammar(language: 'json', variables: JSONSchema): string;
+// function buildGrammar(language: SupportedLanguage, options: JSONSchema | Variables<'sql'>) {
+//   if (isLanguage(language, 'sql')) {
+//     throw new Error('Not yet implemented');
+//     // const sqlVariables = Object.entries(parseSQLVariables(options));
+//     // return Mustache.render(grammarTemplate, sqlVariables.reduce((obj, [key, value,]) => {
+//     //   if (value.length === 0) {
+//     //     return obj;
+//     //   }
+//     //   return {
+//     //     ...obj,
+//     //     [key]: value.map(v => JSON.stringify(v)).join(" | "),
+//     //   };
+//     // }, {
+//     //   selectlist: 'string',
+//     //   tablename: 'string',
+//     // }));
+//   } else if (isLanguage(language, 'json')) {
+//     return JSON2GBNF(options);
+//   }
+//   // return grammarTemplate;
+// };
 
-export function getGrammar<L extends SupportedLanguage>(
-  language: L,
-  variables?: Variables<L>,
-): string {
-  return buildGrammar<L>(language, GRAMMARS[language], variables);
-}
+// const GRAMMARS: Record<SupportedLanguage, string> = {
+//   sql: SQL_GRAMMAR as string,
+//   // javascript: JAVASCRIPT_GRAMMAR,
+//   // python: PYTHON_GRAMMAR,
+//   json: JSON_GRAMMAR as string,
+// };
+
+// export function getGrammar<L extends SupportedLanguage>(
+//   language: L,
+//   variables?: Variables<L>,
+// ): string {
+//   return 'fo';
+//   // return buildGrammar<L>(language, GRAMMARS[language], variables);
+// }
