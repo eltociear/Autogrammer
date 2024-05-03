24e4df4bad5704909a57631c264efe33ced0b462
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-05-03 09:00:16 -0400

    Misc fixes to build processes (#57)
    - Ensure .gbnf files are copied into dist for autogrammer
    - Update local dependency linking
    - Add dev entry for autogrammer

--- packages/autogrammer/javascript/src/autogrammer.ts
+++ packages/autogrammer/javascript/src/autogrammer.ts
@@ -1,12 +1,27 @@
-import Contortionist, { ExternalExecuteOptions, ModelProtocol, } from 'contort';
-import { getGrammar, } from './grammars/index.js';
-import { ConstructorOptions, SUPPORTED_LANGUAGES, SupportedLanguage, isSupportedLanguage, } from './types.js';
+import Contortionist, {
+  type ExternalExecuteOptions,
+  type ModelDefinition,
+  type ModelProtocol,
+} from 'contort';
+import {
+  getGrammar,
+} from './grammars/index.js';
+import type {
+  ConstructorOptions,
+  SupportedLanguage,
+} from './types.js';
 import { buildPrompt, } from './utils.js';
 import { Variables, } from './grammars/get-grammar.js';
+import {
+  isSupportedLanguage,
+} from './type-guards.js';
+import {
+  SUPPORTED_LANGUAGES,
+} from './constants.js';
 
-export class Autogrammer<L extends SupportedLanguage> {
-  language: L;
-  contortionist: Contortionist<ModelProtocol>;
+export class Autogrammer {
+  #language: SupportedLanguage;
+  #contortionist: Contortionist<ModelProtocol>;
 
   /**
    * Instantiates an instance of Autogrammer.
@@ -22,18 +37,38 @@ export class Autogrammer<L extends SupportedLanguage> {
    * 
    * @returns an instance of a Autogrammer class.
    */
-  constructor({ language, model, }: ConstructorOptions<L>) {
+  constructor({ language, model, }: ConstructorOptions<SupportedLanguage>) {
     if (!isSupportedLanguage(language)) {
       throw new Error(`Unsupported language: ${language as string}. Only one of ${JSON.stringify(SUPPORTED_LANGUAGES)} are supported.`);
     }
-    this.language = language;
-    this.contortionist = new Contortionist({
+    this.#language = language;
+    this.#contortionist = new Contortionist({
       model,
-      grammar: getGrammar<L>(language),
+      grammar: getGrammar(language),
     });
   }
 
-  public async synthesize(
+  get language(): SupportedLanguage {
+    if (!isSupportedLanguage(this.#language)) {
+      throw new Error(`Unsupported language: ${this.#language as string}. Only one of ${JSON.stringify(SUPPORTED_LANGUAGES)} are supported.`);
+    }
+    return this.#language;
+  }
+  set language(language: SupportedLanguage) {
+    if (!isSupportedLanguage(language)) {
+      throw new Error(`Unsupported language: ${language as string}. Only one of ${JSON.stringify(SUPPORTED_LANGUAGES)} are supported.`);
+    }
+    this.#language = language;
+  }
+
+  set model(model: ModelDefinition<ModelProtocol>) {
+    this.#contortionist = new Contortionist({
+      model,
+      grammar: getGrammar(this.language),
+    });
+  }
+
+  public async synthesize<L extends SupportedLanguage>(
     prompt: string,
     {
       languageOptions,
@@ -44,8 +79,8 @@ export class Autogrammer<L extends SupportedLanguage> {
     },
   ): Promise<string> {
     if (languageOptions !== undefined) {
-      if (this.language === 'sql') {
-        this.contortionist.grammar = getGrammar<'sql'>(this.language, languageOptions);
+      if (this.#language === 'sql') {
+        this.#contortionist.grammar = getGrammar<'sql'>(this.#language, languageOptions);
         // console.log(this.contortionist.grammar);
         // } else if (this.language === 'json') {
         //   const grammar = await compile(languageOptions, 'Root');
@@ -55,13 +90,13 @@ export class Autogrammer<L extends SupportedLanguage> {
         throw new Error('I dont know this one');
       }
     }
-    const builtPrompt = buildPrompt(prompt, this.language);
-    return await this.contortionist.execute(builtPrompt, {
+    const builtPrompt = buildPrompt(prompt, this.#language);
+    return await this.#contortionist.execute(builtPrompt, {
       ...modelOptions,
     });
   };
 
   abort = () => {
-    this.contortionist.abort();
+    this.#contortionist.abort();
   };
 }
