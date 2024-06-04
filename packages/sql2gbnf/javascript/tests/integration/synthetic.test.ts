378f0571535ebf84a9f606d1a5d44426810bf38c
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-06-04 07:05:56 -0400

    Continue refactoring to use sql v2 (#116)
    Continue refactoring

--- packages/sql2gbnf/javascript/tests/integration/synthetic.test.ts
+++ packages/sql2gbnf/javascript/tests/integration/synthetic.test.ts
@@ -11,15 +11,6 @@ import GBNF from 'gbnf';
 import { noSchemaTests } from './no-schema-tests/index.js';
 
 describe('synthetic, no schema', () => {
-  // test('it parses schema to grammar', () => {
-  //   const grammar = SQL2GBNF();
-  //   expect(grammar).toEqual([
-  //     ...NO_SCHEMA_GRAMMAR,
-  //     ...GBNF_GLOBAL_CONSTANTS,
-  //     ...GLOBAL_CONSTANTS
-  //   ].join('\n'));
-  // });
-
   test.each(noSchemaTests)('it parses schema to grammar with input "%s"', (initial) => {
     const grammar = SQL2GBNF(undefined, {
       whitespace: 'verbose',
