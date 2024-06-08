a4f1f25e6a892ac02bdcd4053a14d00452354fa6
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-06-08 15:36:09 -0400

    JSON2GBNF refactor to leverage new builder template tags (#130)
    * JSON2GBNF refactor to leverage new builder template tags
    
    - Refactor gbnf to support arrays of rules in a template string, allowing for writing a number of rules in one go
    - Add the ability to specify a separator when dealing with arrays
    - Lots of small improvements to GBNF under the hood to deal more precisely with raw strings and arrays

---
+++ packages/gbnf/javascript/src/builder-v2/get-rule-names.test.ts
@@ -0,0 +1,83 @@
+import {
+  describe,
+  expect,
+  test,
+  vi,
+} from 'vitest';
+import {
+  getRuleNames,
+} from './get-rule-names.js';
+import { GrammarBuilder } from './grammar-builder.js';
+import {
+  _,
+} from '../builder-v2/template-tags.js';
+
+const parser = new GrammarBuilder();
+
+describe('getRuleNames', () => {
+  test('it gets rule names for strings', () => {
+    expect(getRuleNames(
+      ['a', 'b'],
+      parser,
+      'default',
+    )).toEqual(['a', 'b']);
+  });
+
+  test('it gets rule names for rules', () => {
+    expect(getRuleNames(
+      [_`a`.key('a1'), _`b`.key('b1')],
+      parser,
+      'default',
+    )).toEqual(['a1', 'b1']);
+  });
+
+  test('it skips undefined', () => {
+    expect(getRuleNames(
+      ['a', undefined, 'b'],
+      parser,
+      'default',
+    )).toEqual(['a', 'b']);
+  });
+
+  test('it returns a mixed group', () => {
+    expect(getRuleNames(
+      ['foo', _`a`.key('a1'), undefined, _`b`.key('b1')],
+      parser,
+      'default',
+    )).toEqual(['foo', 'a1', 'b1']);
+  });
+
+  describe('arrays', () => {
+    test.each([
+      [
+        [
+          'foo',
+          _`a`.key('a1'),
+          undefined,
+          [
+            _`c`.key('c1'),
+            _`d`.key('d1'),
+          ],
+          _`b`.key('b1'),
+        ],
+        ['foo', 'a1', 'x', 'b1'],
+        [
+          'a1 ::= a',
+          'b1 ::= b',
+          'c1 ::= c',
+          'd1 ::= d',
+          'x ::= c1 d1',
+        ],
+      ],
+
+    ])(`(array) it handles '%s'`, (rules, expectation, grammar) => {
+      const ruleNames = getRuleNames(
+        rules,
+        parser,
+        'default',
+      );
+      expect(ruleNames.sort()).toEqual(expectation.sort());
+      expect([...parser.grammar].sort()).toEqual(grammar.sort());
+    });
+  });
+});
